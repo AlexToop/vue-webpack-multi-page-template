0325f8e034e68b585685717628261ea231c470c4
AlexToop <alextoop@outlook.com>
2020-03-22 17:21:31 +0000

    Linted the webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,42 +1,42 @@
-const path = require("path");
-const CopyPlugin = require("copy-webpack-plugin");
-const VueLoaderPlugin = require("vue-loader/lib/plugin");
+const CopyPlugin = require('copy-webpack-plugin')
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
+const path = require('path')
 
 module.exports = {
   entry: {
-    index: "./src/main.js",
-    about: "./src/about/main.js"
+    index: './src/main.js',
+    about: './src/about/main.js'
   },
   module: {
     rules: [
       {
         test: /\.vue$/,
-        loader: "vue-loader"
+        loader: 'vue-loader'
       },
       {
         test: /\.css$/,
-        use: ["vue-style-loader", "css-loader"]
+        use: ['vue-style-loader', 'css-loader']
       }
     ]
   },
   plugins: [
     new VueLoaderPlugin(),
     new CopyPlugin([
-      { from: "src/index.html", to: "../index.html" },
-      { from: "src/about/index.html", to: "../about" },
-      { from: "src/assets/img/*", to: "../img/", flatten: true },
-      { from: "src/assets/favicon/*", to: "../", flatten: true }
+      { from: 'src/index.html', to: '../index.html' },
+      { from: 'src/about/index.html', to: '../about' },
+      { from: 'src/assets/img/*', to: '../img/', flatten: true },
+      { from: 'src/assets/favicon/*', to: '../', flatten: true }
     ])
   ],
   output: {
-    filename: "[name].js",
-    path: path.resolve(__dirname, "build/js")
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'build/js')
   },
   devServer: {
-    contentBase: "build",
+    contentBase: 'build',
     compress: false,
     watchContentBase: true,
-    publicPath: "/js/",
+    publicPath: '/js/',
     port: 1234
   }
-};
+}
